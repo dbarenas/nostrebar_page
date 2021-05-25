5f55d80746df0f79fd292357035d6d226561933b
David Arenas <davidarenas@Davids-MacBook-Pro.local>
2021-05-26 01:26:04 +0200

    detail the bottom

--- assets/lacapelette/js/lacapelette.js
+++ assets/lacapelette/js/lacapelette.js
@@ -1,22 +1,6 @@
 //jQuery to collapse the navbar on scroll
 //***********************************************************************
-$(window).scroll(function() {
-    "use strict";
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-//jQuery for page scrolling feature - requires jQuery Easing plugin
-//***********************************************************************
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+ 
 
 // Gallery and Gallery Filter Styles 
 //***********************************************************************
